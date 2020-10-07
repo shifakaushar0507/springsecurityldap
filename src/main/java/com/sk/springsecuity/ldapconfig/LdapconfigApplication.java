6b13efc8476a7b7ac09ae1721034744d491c115f
shifa kaushar <shifa.kaushar57@gmail.com>
2020-10-07 12:15:35 +0530

    template or initial commit spring security

---
+++ src/main/java/com/sk/springsecuity/ldapconfig/LdapconfigApplication.java
@@ -0,0 +1,13 @@
+package com.sk.springsecuity.ldapconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LdapconfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LdapconfigApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sk/springsecuity/ldapconfig/LdapconfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sk.springsecuity.ldapconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LdapconfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
